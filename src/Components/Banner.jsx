8cc27182994d6e28e8de78d41b1299d4abbbcf8d
Alen Chacko <alenchacko.off@gmail.com>
2023-12-27 12:01:22 +0530

    jobs filtered 1.37

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = ({ query, handleInputChange }) => {
       </p>
       <form>
         <div className="flex justify-start md:flex-row flex-col md:gap-0 gap-4">
-          <div className="flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset  focus-within:ring-2 focus-within:ring-inset  focus-within:ring-indigo-600 md:w-1/3 w-full">
+          <div className="flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset  focus-within:ring-2 focus-within:ring-inset  focus-within:ring-indigo-600 md:w-1/2 w-full">
             <input
               placeholder="What position are you looking for?"
               type="text"
@@ -28,7 +28,7 @@ const Banner = ({ query, handleInputChange }) => {
             />
             <FaSearch className="absolute mt-2.5 ml-2 text-gray-400" />
           </div>
-          <div className="flex md:rounded-s-none rounded shadow-sm ring-1 ring-inset  focus-within:ring-2 focus-within:ring-inset  focus-within:ring-indigo-600 md:w-1/2 w-full">
+          <div className="flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset  focus-within:ring-2 focus-within:ring-inset  focus-within:ring-indigo-600 md:w-1/3 w-full">
             <input
               placeholder="Location"
               type="text"
@@ -37,11 +37,15 @@ const Banner = ({ query, handleInputChange }) => {
               className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900
                 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6
                 "
-              value={""}
             />
             <FaLocationDot className="absolute mt-2.5 ml-2 text-gray-400" />
           </div>
-          <button type="submit" className="py-2 px-8 bg-blue text-white md:rounded-s-none rounded">Search</button>
+          <button
+            type="submit"
+            className="py-2 px-8 bg-blue text-white md:rounded-s-none rounded"
+          >
+            Search
+          </button>
         </div>
       </form>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
-import Banner from '../Components/Banner'
+import React, { useEffect, useState } from "react";
+import Banner from "../Components/Banner";
+import axios from "axios";
 
 const Home = () => {
-  const [query,setQuery] = useState("")
-    const handleInputChange = (event) =>{
-        setQuery(event.target.value)
-    }
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
+  }, []);
+  console.log("jobs<<<<<<",jobs)
+  const [query, setQuery] = useState("");
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+  const filteredItems = jobs.filter((job)=>job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+  console.log("filteredItems>>>>>>>>>>>>",filteredItems)
   return (
     <div>
       <Banner query={query} handleInputChange={handleInputChange} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
